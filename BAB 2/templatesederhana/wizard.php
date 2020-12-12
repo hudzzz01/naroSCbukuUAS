31ad03675afa7f1a6a069de32adc01b88b3dea27
hudzzz01 <57126264+hudzzz01@users.noreply.github.com>
2020-12-12 19:34:41 +0700

    Add files via upload

---
+++ BAB 2/templatesederhana/wizard.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TI UIN JKT</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="page">
+		<div id="sambutan">
+			<h1>Silahkan Masuk</h1>
+			  <h2><marquee>Wahai Para Calon Cendikiawan yang Luar Biasa</marquee></h2> 
+		</div>
+
+		<div id = "loginbg">
+		
+		<form method="post" action="">
+
+			
+			<ul>
+				<li>
+					<label for="username" >Username</label>
+					<input type="Text" name="username" id="username">
+				</li>
+				<li>
+					<label for="password" >Password</label>
+					<input type="password" name="password" id="password">
+				</li>
+				<li>
+					<button name="login">Login</button>
+				</li>
+				<li>
+					<button name="daftar">daftar</button>
+				</li>
+			</ul>
+			</div>
+		</form>
+
+		<br>
+
+		<a href="index.php" class="button">Kembali</a>
+
+	</div>
+</body>
+</html>
